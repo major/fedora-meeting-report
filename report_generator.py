f5fc2bd49ba9a82ff5bb4b7a2085bddb44008ad9
David Kaufmann <astra@ionic.at>
2015-09-16 04:20:52 +0200

    add database support

--- report_generator.py
+++ report_generator.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
 from bugzilla import Bugzilla
 from collections import Counter
-from datetime import datetime
+from datetime import datetime, date
 from terminaltables import UnixTable
+from sqlalchemy import create_engine, func, select, Table, Column, Integer, String, Date, MetaData
 
+engine = create_engine('sqlite:///sqlite3.db', echo=False)
+metadata = MetaData()
+
+bugcounts = Table('bugcounts', metadata,
+    Column('reportdate', Date()),
+    Column('bugtable', String(16)),
+    Column('category', String(32)),
+    Column('owned', Integer),
+    Column('unowned', Integer))
+
+metadata.create_all(engine)
+conn = engine.connect()
 
 TABLES = [
         ('priority', 'Priority'),
@@ -15,6 +28,13 @@ TABLES = [
 
 VALID_STATUSES = ['NEW', 'ASSIGNED', 'MODIFIED', 'ON_QA']
 
+# check if db contains current date - if yes do not collect data
+curdate = date.today()
+collect_data = False
+sel = select([func.count(bugcounts.c.reportdate).label('reportdates')]).where(bugcounts.c.reportdate == curdate)
+cnt = conn.execute(sel).fetchone()[0]
+if (cnt == 0):
+    collect_data = True
 
 def get_security_bugs():
     bz = Bugzilla(url='https://bugzilla.redhat.com/xmlrpc.cgi')
@@ -43,6 +63,7 @@ def build_table(bugs, key, label, limit=None):
     headers = [[label, 'Count', 'Owned', 'Unowned']]
     # Let's build rows for the table
     rows = []
+    tupels = []
     for bug_key, total in sorted(frequency.items(), key=lambda t: t[1],
                                  reverse=True):
         row = [
@@ -52,9 +73,16 @@ def build_table(bugs, key, label, limit=None):
             str(owned[bug_key][False])
             ]
         rows.append(row)
+        if collect_data:
+            dbtupel = dict(zip(['reportdate', 'bugtable', 'category', 'owned', 'unowned'],
+                            [curdate, key, bug_key, owned[bug_key][True], owned[bug_key][False]]))
+            tupels.append(dbtupel)
 
     if limit is not None:
         rows = rows[0:limit]
+
+    if collect_data:
+        conn.execute(bugcounts.insert(), tupels)
 
     # Generate the table
     return UnixTable(table_data=headers+rows,
