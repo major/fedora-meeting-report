3d44332cfbdd7780be0c47acf4f14255d1eec6fb
David Kaufmann <astra@ionic.at>
2015-09-18 03:20:26 +0200

    split up functionality into --cron, --fetch and --date

--- report_generator.py
+++ report_generator.py
@@ -2,9 +2,28 @@
 from bugzilla import Bugzilla
 from collections import Counter
 from datetime import datetime, date
+from time import mktime
 from terminaltables import UnixTable
-from sqlalchemy import create_engine, func, select, Table, Column, Integer, String, Date, MetaData
+from sqlalchemy import create_engine, func, and_, select, Table, Column, Integer, String, Date, MetaData
+import parsedatetime
 import argparse
+import sys
+
+TABLES = [
+        ('priority', 'Priority'),
+        ('status', 'Status'),
+        ('severity', 'Severity'),
+        ('component', 'Component'),
+        ('version', 'Distro Version')
+        ]
+
+metadata = MetaData()
+bugtable = Table('bugs', metadata,
+    Column('date', Date(), primary_key=True),
+    Column('attribute', String(16), primary_key=True),
+    Column('category', String(32), primary_key=True),
+    Column('owned', Integer),
+    Column('unowned', Integer))
 
 def get_security_bugs():
     bz = Bugzilla(url='https://bugzilla.redhat.com/xmlrpc.cgi')
@@ -19,96 +38,134 @@ def get_security_bugs():
     bugs = bz.query(query_data)
     return bugs
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--cron', action='store_true', help='fetch and store date from bugzilla')
-    group.add_argument('--fetch', action='store_true', help='fetch report from bugzilla')
-    group.add_argument('--date', nargs=argparse.REMAINDER, help='show tables from specified date')
-    args = parser.parse_args()
+def database(metadata):
+    engine = create_engine('sqlite:///sqlite3.db', echo=False)
+    metadata.create_all(engine)
+    return engine.connect()
 
-engine = create_engine('sqlite:///sqlite3.db', echo=False)
-metadata = MetaData()
-
-bugcounts = Table('bugcounts', metadata,
-    Column('reportdate', Date(), primary_key=True),
-    Column('bugtable', String(16), primary_key=True),
-    Column('category', String(32), primary_key=True),
-    Column('owned', Integer),
-    Column('unowned', Integer))
-
-metadata.create_all(engine)
-conn = engine.connect()
-
-TABLES = [
-        ('priority', 'Priority'),
-        ('status', 'Status'),
-        ('severity', 'Severity'),
-        ('component', 'Component'),
-        ('version', 'Distro Version')
-        ]
-
-# check if db contains current date - if yes do not collect data
-curdate = date.today()
-collect_data = False
-sel = select([func.count(bugcounts.c.reportdate).label('reportdates')]).where(bugcounts.c.reportdate == curdate)
-cnt = conn.execute(sel).fetchone()[0]
-if (cnt == 0):
-    collect_data = True
-
-def build_table(bugs, key, label, limit=None):
-    # Find count of tickets based on our key
-    frequency = Counter([getattr(x, key) for x in bugs])
+def build_table(bugs, attribute):
+    # Find count of tickets based on our attribute
+    frequency = Counter([getattr(x, attribute) for x in bugs])
 
     # How many tickets of each type are present?
     owned = {}
-    for bug_key in frequency.keys():
+    for category in frequency.keys():
         freq = Counter([('fst_owner' in x.status_whiteboard) for x in bugs
-                       if getattr(x, key) == bug_key])
-        owned[bug_key] = freq
+                       if getattr(x, attribute) == category])
+        owned[category] = freq
 
-    headers = [[label, 'Count', 'Owned', 'Unowned']]
     # Let's build rows for the table
     rows = []
-    tupels = []
-    for bug_key, total in sorted(frequency.items(), key=lambda t: t[1],
+    for category, total in sorted(frequency.items(), key=lambda t: t[1],
                                  reverse=True):
         row = [
-            bug_key,
+            category,
             str(total),
-            str(owned[bug_key][True]),
-            str(owned[bug_key][False])
+            str(owned[category][True]),
+            str(owned[category][False])
             ]
         rows.append(row)
-        if collect_data:
-            dbtupel = dict(zip(['reportdate', 'bugtable', 'category', 'owned', 'unowned'],
-                            [curdate, key, bug_key, owned[bug_key][True], owned[bug_key][False]]))
-            tupels.append(dbtupel)
+    return rows
 
+def save_table(rows, attribute, conn, table):
+    fields = ['date', 'attribute', 'category', 'owned', 'unowned']
+    tupels = []
+    # row = [category, total, owned, unowned]
+    for row in rows:
+        line = [curdate, attribute, row[0], row[2], row[3]]
+        tupels.append(dict(zip(fields, line)))
+    conn.execute(bugtable.insert(), tupels)
+
+def draw_table(rows, label, limit=None):
     if limit is not None:
         rows = rows[0:limit]
 
-    if collect_data:
-        conn.execute(bugcounts.insert(), tupels)
-
+    headers = [[label, 'Tickets', 'Owned', 'Unowned']]
     # Generate the table
     return UnixTable(table_data=headers+rows,
                      title="Tickets by {0}".format(label)).table
 
-# Gather data
-bugs = get_security_bugs()
+def draw_header(datadate):
+    # Build Report
+    datestring = datetime.now().isoformat(' ')
+    datastring = datadate.isoformat()
+    return """
+     __           _
+    / _|  ___  __| | ___  _ __ __ _
+    | |_ / _ \/ _` |/ _ \| '__/ _` |  Fedora Security Team Report
+    |  _|  __/ (_| | (_) | | | (_| |  Report date: {0}
+    |_|  \___|\__,_|\___/|_|  \__,_|  Data from: {1}
+    -------------------------------------------------------------------------------
+    """.format(datestring, datastring)
 
-# Build Report
-datestring = datetime.now().isoformat(' ')
-print """
-  __          _
- / _| ___  __| | ___  _ __ __ _
-| |_ / _ \/ _` |/ _ \| '__/ _` |  Fedora Security Team Report
-|  _|  __/ (_| | (_) | | | (_| |  Report date: {0}
-|_|  \___|\__,_|\___/|_|  \__,_|
--------------------------------------------------------------------------------
-""".format(datestring)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-c', '--cron', action='store_true', help='fetch and store date from bugzilla')
+    group.add_argument('-f', '--fetch', action='store_true', help='fetch report from bugzilla')
+    group.add_argument('-d', '--date', nargs=argparse.OPTIONAL, metavar='date', help='show tables from specified date (e.g. "2 days ago", "today" or "2015-09-01")')
+    args = parser.parse_args()
 
-for table in TABLES:
-    print build_table(bugs, table[0], table[1], 10)
-    print
+    readdate = date.today()
+    if args.date is not None:
+        try:
+            # parse dates like "2015-09-01"
+            readdate = datetime.strptime(args.date, "%Y-%m-%d").date()
+        except ValueError, e:
+            try:
+                # parse all human readable dates
+                cal = parsedatetime.Calendar()
+                time_struct, parse_status = cal.parse(args.date)
+                readdate = datetime.fromtimestamp(mktime(time_struct)).date()
+            except ValueError, e:
+                parser.error("could not parse specified date.")
+    curdate = date.today()
+
+    conn = None
+    if not args.fetch:
+        conn = database(metadata)
+
+    if args.cron:
+        """ Saving bugzilla data to database """
+        # check if db contains current date - if yes do not collect data
+        sel = select([func.count(bugtable.c.date).label('dates')]).where(bugtable.c.date == curdate)
+        if conn.execute(sel).fetchone()[0] == 0:
+            bugs = get_security_bugs()
+            for table in TABLES:
+                rows = build_table(bugs, table[0])
+                # Store data
+                save_table(rows, table[0], conn, bugtable)
+        else:
+            print "Data has been stored already."
+            sys.exit(1)
+
+    elif args.fetch:
+        """ Fetching data from Database """
+        print draw_header(datadate=date.today())
+        # Gather data
+        bugs = get_security_bugs()
+        for table in TABLES:
+            rows = build_table(bugs, table[0])
+            print draw_table(rows, table[1], 10)
+
+    else:
+        """ Get data from specific date """
+        print draw_header(datadate=readdate)
+
+        for table in TABLES:
+            # fetch db table from $readdate
+            rows = []
+            sel = select(
+                    [bugtable.c.category,bugtable.c.owned, bugtable.c.unowned]
+                    ).where(
+                            and_(
+                                bugtable.c.date == readdate,
+                                bugtable.c.attribute == table[0]
+                                )
+                            )
+            for elem in conn.execute(sel).fetchall():
+                rows.append([elem[0], str(elem[1]+elem[2]), str(elem[1]), str(elem[2])])
+            print draw_table(rows, table[1], 10)
+
+    if conn is not None:
+        conn.close()
